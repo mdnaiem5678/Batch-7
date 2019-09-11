5e90c287f46da1ddb44733effdc0722f5f4ef464
IH Naim <mdnaiem5678@gmail.com>
2019-09-12 02:44:10 +0600

    ADDproject

---
+++ Basic C_Sharp/Basic C_Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "";
+            string Age = " ";
+            string Address = " ";
+            string Contact = " ";
+
+            Console.WriteLine("Enter Your name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Your Name is : " + Name);
+
+            Console.WriteLine("Enter Your Age : ");
+            Age = Console.ReadLine();
+            Console.WriteLine("Your Age is : " + Age);
+
+            Console.WriteLine("Enter your Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Your Address is: " + Address);
+
+            Console.WriteLine("Enter Your Contact: ");
+            Contact = Console.ReadLine();
+            Console.WriteLine("Your Contact is: " + Contact);
+            Console.ReadKey();
+
+        }
+    }
+}
